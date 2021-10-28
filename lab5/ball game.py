e555d283799f4ba63035a1cf3b6321c258e4bb08
alexanderkudryashov3 <kudryashovalexander9@gmail.com>
2021-10-28 21:08:10 +0400

    Добавлен новый враг

--- lab5/ball game.py
+++ lab5/ball game.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame.draw import *
+from random import random
 from random import randint
 pygame.init()
 
 FPS = 60
-screen = pygame.display.set_mode((1200, 900))
+screen = pygame.display.set_mode((1200, 800))
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -19,10 +20,12 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-points, num_balls = 0, 20
+points, num_balls, num_squares = 0, 10, 1
 x, y, r, v_x, v_y, colors = [0] * num_balls, [0] * num_balls, [0] * num_balls,\
                             [0] * num_balls, [0] * num_balls, [0] * num_balls
 
+x2, y2, r2, v_x2, v_y2, colors2 = [0] * num_balls, [0] * num_balls, [0] * num_balls,\
+                            [0] * num_balls, [0] * num_balls, [0] * num_balls
 
 def generate_new_ball(i):
     '''
@@ -34,7 +37,17 @@ def generate_new_ball(i):
     r[i] = randint(30, 50)
     v_x[i] = randint(-10, +10)
     v_y[i] = randint(-10, +10)
-    colors[i] = COLORS[randint(0, 6)]
+    colors[i] = COLORS[randint(0, 3)]
+
+
+def generate_new_square(i):
+    global x2, y2, r2, v_x2, v_y2, colors2
+    x2[i] = randint(0, 1200)
+    y2[i] = randint(0, 800)
+    r2[i] = randint(50, 150)
+    v_x2[i] = randint(-10, +10)
+    v_y2[i] = randint(-10, +10)
+    colors2[i] = COLORS[randint(4, 6)]
 
 
 def draw_balls(x, y, r, colors):
@@ -42,27 +55,54 @@ def draw_balls(x, y, r, colors):
         circle(screen, colors[i], (x[i], y[i]), r[i])
 
 
+def draw_squares(x2, y2, r2, colors2):
+    for i in range(num_squares):
+        rect(screen, colors2[i], (x2[i], y2[i], r2[i], r2[i]))
+
+
 def click_handler(event):
     global points
     x1, y1 = event.pos
     click_inside_ball = [r[i]**2>(x[i]-x1)**2+(y[i]-y1)**2 for i in range(num_balls)]
+    click_inside_square = [r2[i] + x2[i] > x1 > x2[i] and r2[i] + y2[i] > y1 > y2[i] for i in range(num_squares)]
     for i in range(num_balls):
         if click_inside_ball[i]:
             generate_new_ball(i)
-    points += sum(click_inside_ball)
+    for i in range(num_squares):
+        if click_inside_square[i]:
+            generate_new_square(i)
+    points += sum(click_inside_ball)+5*sum(click_inside_square)
+
+
+
 
 
 def check_wall():
     global x, y, r, v_x, v_y, num_balls
-    hit_right_wall = [1120 - x[i] + r[i] <= v_x[i] and v_x[i] > 0 for i in range(num_balls)]
+    global x2, y2, r2, v_x2, v_y2, num_squares
+    hit_right_wall = [1200 - x[i] - r[i] <= v_x[i] and v_x[i] > 0 for i in range(num_balls)]
     hit_left_wall = [x[i] - r[i] <= abs(v_x[i]) and v_x[i] < 0 for i in range(num_balls)]
     hit_upper_wall = [y[i] - r[i] <= abs(v_y[i]) and v_y[i] < 0 for i in range(num_balls)]
-    hit_lower_wall = [900 - y[i] - r[i] <= v_y[i] and v_y[i] > 0 for i in range(num_balls)]
+    hit_lower_wall = [800 - y[i] - r[i] <= v_y[i] and v_y[i] > 0 for i in range(num_balls)]
+
+    hit_right_wall2 = [1200 - x2[i] - r2[i] <= 2*v_x2[i] and v_x2[i] > 0 for i in range(num_squares)]
+    hit_left_wall2 = [x2[i] <= 2*abs(v_x2[i]) and v_x2[i] < 0 for i in range(num_squares)]
+    hit_upper_wall2 = [y2[i] <= 2*abs(v_y2[i]) and v_y2[i] < 0 for i in range(num_squares)]
+    hit_lower_wall2 = [800 - y2[i] - r2[i] <= 2*v_y2[i] and v_y2[i] > 0 for i in range(num_squares)]
+
     for i in range(num_balls):
         if hit_right_wall[i] or hit_left_wall[i]:
             v_x[i] *= -1
         if hit_upper_wall[i] or hit_lower_wall[i]:
             v_y[i] *= -1
+
+    for i in range(num_squares):
+        if hit_right_wall2[i] or hit_left_wall2[i]:
+            v_x2[i] *= -1
+        if hit_upper_wall2[i] or hit_lower_wall2[i]:
+            v_y2[i] *= -1
+
+
 
 
 def display_points():
@@ -78,8 +118,24 @@ def balls_position_update():
     y = [y[i] + v_y[i] for i in range(num_balls)]
 
 
+def squares_position_update():
+    global x2, y2, v_x2, v_y2
+    for i in range(num_squares):
+        if v_x2[i] > 30:
+            v_x2[i] = 5
+        if v_y2[i] > 30:
+            v_y2[i] = 5
+    v_x2 = [v_x2[i]*(randint(499, 503)/500) for i in range(num_squares)]
+    v_y2 = [v_y2[i]+(randint(-1, 1)) for i in range(num_squares)]
+
+    x2 = [x2[i] + v_x2[i] for i in range(num_squares)]
+    y2 = [y2[i] + v_y2[i] for i in range(num_squares)]
+
+
 for i in range(num_balls):
     generate_new_ball(i)
+for i in range(num_squares):
+    generate_new_square(i)
 
 while not finished:
     clock.tick(FPS)
@@ -91,9 +147,11 @@ while not finished:
 
     check_wall()
     balls_position_update()
+    squares_position_update()
     screen.fill(BLACK)
     display_points()
     draw_balls(x, y, r, colors)
+    draw_squares(x2, y2, r2, colors2)
     pygame.display.update()
 
 pygame.quit()
